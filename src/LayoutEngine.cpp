238d1afa7c3733f76c3a918253aa1f66e92cad19
Behdad Esfahbod <behdad@behdad.org>
2012-10-28 22:55:28 -0700

    Add to/from_float()

--- src/LayoutEngine.cpp
+++ src/LayoutEngine.cpp
@@ -25,6 +25,18 @@ const le_int32 LayoutEngine::kTypoFlagLiga = 0x2;
 
 UOBJECT_DEFINE_RTTI_IMPLEMENTATION(LayoutEngine)
 
+static inline float
+to_float (hb_position_t v)
+{
+  return v;
+}
+
+static inline hb_position_t
+from_float (float v)
+{
+  return v;
+}
+
 
 static hb_blob_t *
 icu_le_hb_reference_table (hb_face_t *face, hb_tag_t tag, void *user_data)
@@ -63,7 +75,7 @@ icu_le_hb_font_get_glyph_h_advance (hb_font_t *font,
 
   fontInstance->getGlyphAdvance (glyph, advance);
 
-  return advance.fX;
+  return from_float (advance.fX);
 }
 
 static hb_bool_t
@@ -81,8 +93,8 @@ icu_le_hb_font_get_glyph_contour_point (hb_font_t *font,
   if (!fontInstance->getGlyphPoint (glyph, point_index, point))
     return false;
 
-  *x = point.fX;
-  *y = point.fY;
+  *x = from_float (point.fX);
+  *y = from_float (point.fY);
 
   return true;
 }
@@ -140,8 +152,8 @@ LayoutEngine::LayoutEngine(const LEFontInstance *fontInstance,
     }
     hb_font_set_funcs (fHbFont, icu_le_hb_get_font_funcs (), (void *) fontInstance, NULL);
     hb_font_set_scale (fHbFont,
-		       +fontInstance->getXPixelsPerEm () * fontInstance->getScaleFactorX (),
-		       -fontInstance->getYPixelsPerEm () * fontInstance->getScaleFactorY ());
+		       +from_float (fontInstance->getXPixelsPerEm () * fontInstance->getScaleFactorX ()),
+		       -from_float (fontInstance->getYPixelsPerEm () * fontInstance->getScaleFactorY ()));
     hb_font_set_ppem (fHbFont,
 		      fontInstance->getXPixelsPerEm (),
 		      fontInstance->getYPixelsPerEm ());
@@ -204,10 +216,10 @@ void LayoutEngine::getGlyphPositions(float positions[], LEErrorCode &success) co
   float x = 0, y = 0;
   unsigned int i;
   for (i = 0; i < count; i++) {
-    positions[2 * i]     = x + pos[i].x_offset;
-    positions[2 * i + 1] = y + pos[i].y_offset;
-    x += pos[i].x_advance;
-    y += pos[i].y_advance;
+    positions[2 * i]     = x + to_float (pos[i].x_offset);
+    positions[2 * i + 1] = y + to_float (pos[i].y_offset);
+    x += to_float (pos[i].x_advance);
+    y += to_float (pos[i].y_advance);
   }
   positions[2 * i]     = x;
   positions[2 * i + 1] = y;
@@ -221,11 +233,11 @@ void LayoutEngine::getGlyphPosition(le_int32 glyphIndex, float &x, float &y, LEE
   unsigned int i;
   x = 0; y = 0;
   for (i = 0; i < (unsigned int) glyphIndex; i++) {
-    x += pos[i].x_advance;
-    y += pos[i].y_advance;
+    x += to_float (pos[i].x_advance);
+    y += to_float (pos[i].y_advance);
   }
-  x += pos[glyphIndex].x_offset;
-  y += pos[glyphIndex].y_offset;
+  x += to_float (pos[glyphIndex].x_offset);
+  y += to_float (pos[glyphIndex].y_offset);
 }
 
 // Input: characters, font?
@@ -242,6 +254,8 @@ le_int32 LayoutEngine::layoutChars(const LEUnicode chars[], le_int32 offset, le_
         success = LE_ILLEGAL_ARGUMENT_ERROR;
         return 0;
     }
+
+    /* XXX save x,y on the engine. */
 
     hb_buffer_set_length (fHbBuffer, 0);
     hb_buffer_set_direction (fHbBuffer, rightToLeft ? HB_DIRECTION_RTL : HB_DIRECTION_LTR);
