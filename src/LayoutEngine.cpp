351603bd30e0e31d42934c59d333fcb4497f4e3d
Behdad Esfahbod <behdad@behdad.org>
2013-11-15 15:14:53 -0500

    Indent

--- src/LayoutEngine.cpp
+++ src/LayoutEngine.cpp
@@ -32,25 +32,25 @@ UOBJECT_DEFINE_RTTI_IMPLEMENTATION(LayoutEngine)
 static inline float
 to_float (hb_position_t v)
 {
-  return scalblnf (v, -8);
+    return scalblnf (v, -8);
 }
 
 static inline hb_position_t
 from_float (float v)
 {
-  return scalblnf (v, +8);
+    return scalblnf (v, +8);
 }
 
 
 static hb_blob_t *
 icu_le_hb_reference_table (hb_face_t *face, hb_tag_t tag, void *user_data)
 {
-  const LEFontInstance *fontInstance = (const LEFontInstance *) user_data;
+    const LEFontInstance *fontInstance = (const LEFontInstance *) user_data;
 
-  size_t length = 0;
-  const char *data = (const char *) fontInstance->getFontTable (tag, length);
+    size_t length = 0;
+    const char *data = (const char *) fontInstance->getFontTable (tag, length);
 
-  return hb_blob_create (data, length, HB_MEMORY_MODE_READONLY, NULL, NULL);
+    return hb_blob_create (data, length, HB_MEMORY_MODE_READONLY, NULL, NULL);
 }
 
 static hb_bool_t
@@ -62,10 +62,10 @@ icu_le_hb_font_get_glyph (hb_font_t *font,
                           void *user_data)
 
 {
-  const LEFontInstance *fontInstance = (const LEFontInstance *) font_data;
+    const LEFontInstance *fontInstance = (const LEFontInstance *) font_data;
 
-  *glyph = fontInstance->mapCharToGlyph (unicode);
-  return !!glyph;
+    *glyph = fontInstance->mapCharToGlyph (unicode);
+    return !!glyph;
 }
 
 static hb_position_t
@@ -74,12 +74,12 @@ icu_le_hb_font_get_glyph_h_advance (hb_font_t *font,
                                     hb_codepoint_t glyph,
                                     void *user_data)
 {
-  const LEFontInstance *fontInstance = (const LEFontInstance *) font_data;
-  LEPoint advance;
+    const LEFontInstance *fontInstance = (const LEFontInstance *) font_data;
+    LEPoint advance;
 
-  fontInstance->getGlyphAdvance (glyph, advance);
+    fontInstance->getGlyphAdvance (glyph, advance);
 
-  return from_float (advance.fX);
+    return from_float (advance.fX);
 }
 
 static hb_bool_t
@@ -91,40 +91,40 @@ icu_le_hb_font_get_glyph_contour_point (hb_font_t *font,
                                         hb_position_t *y,
                                         void *user_data)
 {
-  const LEFontInstance *fontInstance = (const LEFontInstance *) font_data;
-  LEPoint point;
+    const LEFontInstance *fontInstance = (const LEFontInstance *) font_data;
+    LEPoint point;
 
-  if (!fontInstance->getGlyphPoint (glyph, point_index, point))
-    return false;
+    if (!fontInstance->getGlyphPoint (glyph, point_index, point))
+        return false;
 
-  *x = from_float (point.fX);
-  *y = from_float (point.fY);
+    *x = from_float (point.fX);
+    *y = from_float (point.fY);
 
-  return true;
+    return true;
 }
 
 static hb_font_funcs_t *
 icu_le_hb_get_font_funcs (void)
 {
-  static hb_font_funcs_t *ffuncs = NULL;
+    static hb_font_funcs_t *ffuncs = NULL;
 
 retry:
-  if (!ffuncs) {
-    /* Only pseudo-thread-safe... */
-    hb_font_funcs_t *f = hb_font_funcs_create ();
-    hb_font_funcs_set_glyph_func (f, icu_le_hb_font_get_glyph, NULL, NULL);
-    hb_font_funcs_set_glyph_h_advance_func (f, icu_le_hb_font_get_glyph_h_advance, NULL, NULL);
-    hb_font_funcs_set_glyph_contour_point_func (f, icu_le_hb_font_get_glyph_contour_point, NULL, NULL);
+    if (!ffuncs) {
+        /* Only pseudo-thread-safe... */
+        hb_font_funcs_t *f = hb_font_funcs_create ();
+        hb_font_funcs_set_glyph_func (f, icu_le_hb_font_get_glyph, NULL, NULL);
+        hb_font_funcs_set_glyph_h_advance_func (f, icu_le_hb_font_get_glyph_h_advance, NULL, NULL);
+        hb_font_funcs_set_glyph_contour_point_func (f, icu_le_hb_font_get_glyph_contour_point, NULL, NULL);
 
-    if (!ffuncs)
-      ffuncs = f;
-    else {
-      hb_font_funcs_destroy (f);
-      goto retry;
+        if (!ffuncs)
+            ffuncs = f;
+        else {
+            hb_font_funcs_destroy (f);
+            goto retry;
+        }
     }
-  }
 
-  return ffuncs;
+    return ffuncs;
 }
 
 static hb_script_t
@@ -148,7 +148,7 @@ LayoutEngine::LayoutEngine(const LEFontInstance *fontInstance,
                            le_int32 languageCode,
                            le_int32 typoFlags,
                            LEErrorCode &success)
-  : fHbFont(NULL), fHbBuffer(NULL), fGlyphStorage (NULL), fTypoFlags(typoFlags)
+    : fHbFont(NULL), fHbBuffer(NULL), fGlyphStorage (NULL), fTypoFlags(typoFlags)
 {
     if (LE_FAILURE(success)) {
         return;
@@ -216,34 +216,34 @@ le_int32 LayoutEngine::getGlyphCount() const
 
 void LayoutEngine::getCharIndices(le_int32 charIndices[], le_int32 indexBase, LEErrorCode &success) const
 {
-  fGlyphStorage->getCharIndices(charIndices, indexBase, success);
+    fGlyphStorage->getCharIndices(charIndices, indexBase, success);
 }
 
 void LayoutEngine::getCharIndices(le_int32 charIndices[], LEErrorCode &success) const
 {
-  fGlyphStorage->getCharIndices(charIndices, success);
+    fGlyphStorage->getCharIndices(charIndices, success);
 }
 
 // Copy the glyphs into caller's (32-bit) glyph array, OR in extraBits
 void LayoutEngine::getGlyphs(le_uint32 glyphs[], le_uint32 extraBits, LEErrorCode &success) const
 {
-  fGlyphStorage->getGlyphs(glyphs, extraBits, success);
+    fGlyphStorage->getGlyphs(glyphs, extraBits, success);
 }
 
 void LayoutEngine::getGlyphs(LEGlyphID glyphs[], LEErrorCode &success) const
 {
-  fGlyphStorage->getGlyphs(glyphs, success);
+    fGlyphStorage->getGlyphs(glyphs, success);
 }
 
 
 void LayoutEngine::getGlyphPositions(float positions[], LEErrorCode &success) const
 {
-  fGlyphStorage->getGlyphPositions(positions, success);
+    fGlyphStorage->getGlyphPositions(positions, success);
 }
 
 void LayoutEngine::getGlyphPosition(le_int32 glyphIndex, float &x, float &y, LEErrorCode &success) const
 {
-  fGlyphStorage->getGlyphPosition(glyphIndex, x, y, success);
+    fGlyphStorage->getGlyphPosition(glyphIndex, x, y, success);
 }
 
 // Input: characters, font?
@@ -299,7 +299,7 @@ le_int32 LayoutEngine::layoutChars(const LEUnicode chars[], le_int32 offset, le_
     fGlyphStorage->allocatePositions (success);
 
     if (LE_FAILURE(success))
-      return 0;
+        return 0;
 
     unsigned int j = 0;
     iter = start;
@@ -350,13 +350,13 @@ void LayoutEngine::reset()
 
 LayoutEngine *LayoutEngine::layoutEngineFactory(const LEFontInstance *fontInstance, le_int32 scriptCode, le_int32 languageCode, LEErrorCode &success)
 {
-  // 3 -> kerning and ligatures
-  return LayoutEngine::layoutEngineFactory(fontInstance,
-                                           scriptCode,
-                                           languageCode,
-                                           (LayoutEngine::kTypoFlagKern |
-                                            LayoutEngine::kTypoFlagLiga),
-                                           success);
+    // 3 -> kerning and ligatures
+    return LayoutEngine::layoutEngineFactory(fontInstance,
+                                             scriptCode,
+                                             languageCode,
+                                             (LayoutEngine::kTypoFlagKern |
+                                              LayoutEngine::kTypoFlagLiga),
+                                             success);
 }
 
 LayoutEngine *LayoutEngine::layoutEngineFactory(const LEFontInstance *fontInstance, le_int32 scriptCode, le_int32 languageCode, le_int32 typoFlags, LEErrorCode &success)
@@ -369,9 +369,9 @@ LayoutEngine *LayoutEngine::layoutEngineFactory(const LEFontInstance *fontInstan
     result = new LayoutEngine(fontInstance, scriptCode, languageCode, typoFlags, success);
 
     if (result && LE_FAILURE(success)) {
-                delete result;
-                result = NULL;
-        }
+        delete result;
+        result = NULL;
+    }
 
     if (result == NULL) {
         success = LE_MEMORY_ALLOCATION_ERROR;
